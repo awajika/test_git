5f3bf1f5bf6bd9205ff1c02d35f2fecbaf0db3de
tokui.kotone@relation <tokui.kotone@relation2012.com>
2024-10-02 15:26:13 +0900

    [fix][fix/unit_test]レビュー箇所の修正 ステータスコード200を返すよう修正
    該当箇所：商品マスタ一括登録機能のNo.1

--- src/main/java/org/example/controller/ItemController.java
+++ src/main/java/org/example/controller/ItemController.java
@@ -325,7 +325,7 @@ public class ItemController {
     if ((!securitySession.getRole().equals(Role.ADMIN.getUserRole()))) {
       HashMap<String, String> forbidden = new HashMap<>();
       forbidden.put("message", "forbidden");
-      return new ResponseEntity<>(forbidden, HttpStatus.BAD_REQUEST);
+      return new ResponseEntity<>(forbidden, HttpStatus.OK);
     }
 
     // エラーメッセージをリクエストヘッダのmessageにセットするMapを用意
